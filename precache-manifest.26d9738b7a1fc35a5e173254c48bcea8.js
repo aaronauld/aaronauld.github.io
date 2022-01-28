1ff56681a5a25155c90e64da4e2b12c8198b62a0
aaronauld <88762795+aaronauld@users.noreply.github.com>
2022-01-28 21:38:36 +1100

    Updates

---
+++ precache-manifest.26d9738b7a1fc35a5e173254c48bcea8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fc8a289a51a6f2ab75ab13a08c79cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc928ad302b73f879743",
+    "url": "/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "647fbe46a93e755dd3ae",
+    "url": "/static/css/main.7ac38e42.chunk.css"
+  },
+  {
+    "revision": "fc928ad302b73f879743",
+    "url": "/static/js/2.e4293ca1.chunk.js"
+  },
+  {
+    "revision": "940a664b1bcf5723c6f06a43f8e0f1ac",
+    "url": "/static/js/2.e4293ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "647fbe46a93e755dd3ae",
+    "url": "/static/js/main.69511b16.chunk.js"
+  },
+  {
+    "revision": "51b1263d09889fed3508",
+    "url": "/static/js/runtime-main.07ad49ca.js"
+  },
+  {
+    "revision": "71d3655f9205a67d46eca9b15d8e2084",
+    "url": "/static/media/memoji.71d3655f.jpg"
+  }
+]);
